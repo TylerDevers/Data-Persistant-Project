4b473641ce8db5805ee56df8be58f6322b6a54e9
Tyler Devers <TylerDevers@users.noreply.github.com>
2023-02-28 10:50:16 -0800

    bug fixes
    fix MainManager related bug fixes in main scene.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class GameManager : MonoBehaviour
 {
+
+    public static GameManager Instance;
+
+
+
+    private void Awake() {
+        // singleton, ensures only one instance exists.
+        if (Instance != null) {
+            Destroy(gameObject);
+        } else {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +33,17 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void StartGame() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame() {
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+        #else
+            Application.Quit();
+        #endif
     }
 }
